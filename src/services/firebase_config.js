64f7fd72072756d11168dd141032d8b644972884
Juana Llanje <juana.lc@hotmail.es>
2022-08-22 12:14:53 -0500

    Instalando firebase a nuestro proyecto

---
+++ src/services/firebase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDwMSfsrDi14Gvr4WcXPd5IE4U1P8QyHM",
+  authDomain: "minsur-fa571.firebaseapp.com",
+  projectId: "minsur-fa571",
+  storageBucket: "minsur-fa571.appspot.com",
+  messagingSenderId: "1093305209841",
+  appId: "1:1093305209841:web:bb13f7c0705aae169c2a50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

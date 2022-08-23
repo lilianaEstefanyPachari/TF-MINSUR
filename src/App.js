e5e54ee37b07f13d9c59f7566c569309021a010f
Nay Trevejo <nay.trevejo@gmail.com>
2022-08-23 00:22:16 -0500

    time coupon form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
+import TimeCoupon from './components/timeCoupon/TimeCoupon';
 
 function App() {
 	return (
 		<>
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={ <Login />} />
-			</Routes>
-		</BrowserRouter></>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Login />} />
+					<Route path='/formFreeTime' element={<TimeCoupon />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 }
 

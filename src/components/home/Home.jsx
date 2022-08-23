e434ce6ab3bc1522b648d0b4ea09b25480ab1d06
lilianaEstefanyPachari <lilianapachari21@gmail.com>
2022-08-23 09:54:59 -0500

    añadiendo estilos a vista home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,8 +3,16 @@ import styles from './Home.module.css';
 import { Stack } from '@mui/material';
 import Nav from '../nav/Nav';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import { SimpleAccordion } from '../accordion/accordion';
+import icon1 from '../../assets/Frame1.png';
+import icon2 from '../../assets/Frame2.png';
 
 const Home = () => {
+	const beneficios = {
+		description: 'Convenios',
+		title:
+			'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.',
+	};
 	return (
 		<div className={styles.container}>
 			<Stack>
@@ -27,7 +35,52 @@ const Home = () => {
 					sx={{ color: '#ffffff', margin: '3px' }}
 				/>
 			</Stack>
-			<Stack></Stack>
+			<Stack sx={{ background: '#ffffff', borderRadius: '20px 20px 0 0' }}>
+				<Stack direction='row' sx={{ justifyContent: 'space-around' }}>
+					<p className={styles.benefits}>Mis beneficios</p>
+					<p className={styles.projects}>Proyectos y programas</p>
+				</Stack>
+				<Stack sx={{ padding: '10px' }}>
+					<SimpleAccordion
+						src={icon1}
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+					<SimpleAccordion
+						src={icon2}
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+					<SimpleAccordion
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+					<SimpleAccordion
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+					<SimpleAccordion
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+					<SimpleAccordion
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+					<SimpleAccordion
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>{' '}
+					<SimpleAccordion
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+					<SimpleAccordion
+						title='Convenios'
+						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+					></SimpleAccordion>
+				</Stack>
+			</Stack>
 			<Stack></Stack>
 		</div>
 	);

2020f0f357d1f07e89c9020027c05ce5e98d8763
Nay Trevejo <nay.trevejo@gmail.com>
2022-08-22 22:21:32 -0500

    added styles to login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,19 +4,50 @@ import { Link } from 'react-router-dom';
 import { BiUserCircle } from 'react-icons/bi';
 import { MdLockOutline } from 'react-icons/md';
 import Logo from '../../assets/Logo_Minsur.png';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const Login = () => {
 	return (
 		<section>
-			<form>
+			<div className={style.logoContainer}>
 				<img className={style.logo} src={Logo} alt='Logo' />
-				<h1>Ingresar</h1>
-				<BiUserCircle />
-				<input type='email' placeholder='Ingresa tu correo electrónico' />
-				<MdLockOutline />
-				<input type='password' placeholder='Ingresa tu contraseña' />
-				<Link to='#'>¿Olvidaste tu contraseña?</Link>
-				<button>Ingresar</button>
+			</div>
+			<form>
+				{/* <h1>Ingresar</h1> */}
+				<TextField
+					id='outlined-start-adornment'
+					label='Correo electrónico'
+					variant='outlined'
+					type='email'
+					placeholder='Ingresa tu correo electrónico'
+					sx={{ m: 1, width: '30ch' }}
+					InputProps={{
+						startAdornment: (
+							<InputAdornment position='start'>
+								<BiUserCircle />
+							</InputAdornment>
+						),
+					}}
+				/>
+				<TextField
+					id='outlined-password-input'
+					label='Contraseña'
+					type='password'
+					placeholder='Ingresa tu contraseña'
+					sx={{ m: 1, width: '30ch' }}
+					InputProps={{
+						startAdornment: (
+							<InputAdornment position='start'>
+								<MdLockOutline />
+							</InputAdornment>
+						),
+					}}
+				/>
+				<Link className={style.link} to='#'>
+					¿Olvidaste tu contraseña?
+				</Link>
+				<button className={style.submitBtn}>Ingresar</button>
 			</form>
 
 			{/* <div id={style.container}>

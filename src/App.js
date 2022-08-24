fbdc176527e331496055d1d7196b7b48a0b4666d
malev.dg <malev.dg@outlook.com>
2022-08-24 01:42:04 -0500

    interfaz benefit information y description benefit - sin rutas

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Home from './components/home/Home';
 import Login from './components/login/Login';
 import { AuthProvider } from './context/authContext';
 import TimeCoupon from './components/timeCoupon/TimeCoupon';
-import DescriptionBenefit from './components/DescriptionBenefit/DescriptionBenefit';
-import BenefitsInformation from './components/benefitsInformation/BenefitsInformation';
 
 function App() {
 	return (
@@ -16,8 +14,6 @@ function App() {
 						<Route path='/' element={<Login />} />
 						<Route path='/home' element={<Home />} />
 						<Route path='/formFreeTime' element={<TimeCoupon />} />
-						<Route path='/Description' element={ <DescriptionBenefit />} />
-						<Route path='/Information' element={ <BenefitsInformation />} />
 					</Routes>
 				</BrowserRouter>
 			</AuthProvider>

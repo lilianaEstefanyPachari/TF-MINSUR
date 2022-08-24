b4e6bfa1d763d80b11f14bbcc1175816423144d6
lilianaEstefanyPachari <lilianapachari21@gmail.com>
2022-08-23 21:02:24 -0500

    añadiendo estilos de vista home beneficios

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Home.module.css';
 import { Stack } from '@mui/material';
 import Nav from '../nav/Nav';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import { SimpleAccordion } from '../accordion/accordion';
-import icon1 from '../../assets/Frame1.png';
-import icon2 from '../../assets/Frame2.png';
+import icon1 from './iconsHome/icon1.png';
+import icon2 from './iconsHome/icon2.png';
+import icon3 from './iconsHome/icon3.png';
+import icon4 from './iconsHome/icon4.png';
+import icon5 from './iconsHome/icon5.png';
+import icon6 from './iconsHome/icon6.png';
+import icon7 from './iconsHome/icon7.png';
+import icon8 from './iconsHome/icon8.png';
+import icon9 from './iconsHome/icon9.png';
 const Home = () => {
+	// simulando data para props
+	const actionBtn = {
+		opc1: 'Solicitar beneficio',
+		opc2: 'Solicitar beneficioooo',
+	};
+	// manejando vistas
+	const [stateViewOptions, setStateViewOptions] = useState(true);
+	const viewOptionsController = () => {
+		setStateViewOptions(true);
+	};
+	// className={completed ? 'text-strike' : null}
 	return (
 		<div className={styles.container}>
 			<Stack>
@@ -15,14 +33,13 @@ const Home = () => {
 			<Stack
 				direction='row'
 				sx={{
-					height: '90px',
+					height: '80px',
 					justifyContent: 'center',
 					alignItems: 'center',
-					padding: '10px',
 				}}
 			>
 				<p className={styles.greetings}>
-					Hola, <span>usuario!</span>
+					Hola, <span style={{ fontWeight: '600' }}>usuario!</span>
 				</p>
 				<NotificationsNoneIcon
 					fontSize='large'
@@ -30,52 +47,137 @@ const Home = () => {
 				/>
 			</Stack>
 			<Stack sx={{ background: '#FFFFFF', borderRadius: '20px 20px 0 0' }}>
-				<Stack direction='row' sx={{ justifyContent: 'space-around' }}>
-					<p className={styles.benefits}>Mis beneficios</p>
-					<p className={styles.projects}>Proyectos y programas</p>
+				<Stack
+					direction='row'
+					sx={{ justifyContent: 'space-around', padding: '5px' }}
+				>
+					<p
+						onClick={viewOptionsController}
+						className={
+							stateViewOptions ? `${styles.textActive}` : `${styles.proyects}`
+						}
+					>
+						Mis beneficios
+					</p>
+					<p
+						onClick={() => setStateViewOptions(false)}
+						className={
+							!stateViewOptions ? `${styles.textActive}` : `${styles.proyects}`
+						}
+					>
+						Proyectos y programas
+					</p>
 				</Stack>
-				<Stack sx={{ padding: '10px' }}>
-					<SimpleAccordion
-						src={icon1}
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-					<SimpleAccordion
-						src={icon2}
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-					<SimpleAccordion
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-					<SimpleAccordion
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-					<SimpleAccordion
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-					<SimpleAccordion
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-					<SimpleAccordion
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>{' '}
-					<SimpleAccordion
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-					<SimpleAccordion
-						title='Convenios'
-						description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
-					></SimpleAccordion>
-				</Stack>
+				{stateViewOptions && (
+					<Stack sx={{ padding: '10px' }}>
+						<SimpleAccordion
+							actionBtn='Solicitar beneficio'
+							src={icon1}
+							title='Convenios'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+						<SimpleAccordion
+							actionBtns={actionBtn}
+							src={icon2}
+							title='Cupones tiempo libre'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+						<SimpleAccordion
+							src={icon3}
+							title='Cupones de descuentos'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+							actionBtn='Solicitar beneficio'
+						></SimpleAccordion>
+						<SimpleAccordion
+							src={icon4}
+							title='Seguro de salud'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+							actionBtn='Solicitar beneficio'
+						></SimpleAccordion>
+						<SimpleAccordion
+							src={icon5}
+							title='Home office'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+							actionBtn='Solicitar beneficio'
+						></SimpleAccordion>
+						<SimpleAccordion
+							src={icon6}
+							title='Horarios flexibles'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+							actionBtn='Solicitar beneficio'
+						></SimpleAccordion>
+						<SimpleAccordion
+							src={icon7}
+							title='Horario de verano'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+							actionBtn='Solicitar beneficio'
+						></SimpleAccordion>{' '}
+						<SimpleAccordion
+							src={icon8}
+							title='Licencia de paternidad'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+							actionBtn='Solicitar beneficio'
+						></SimpleAccordion>
+						<SimpleAccordion
+							src={icon9}
+							title='Licencia por duelo'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+							actionBtn='Solicitar beneficio'
+						></SimpleAccordion>
+					</Stack>
+				)}
+				{!stateViewOptions && (
+					<Stack sx={{ padding: '10px' }}>
+						<SimpleAccordion
+							actionBtn='participar'
+							src={icon1}
+							title='Programa1'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+						<SimpleAccordion
+							actionBtn='participar'
+							src={icon1}
+							title='Programa2'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+						<SimpleAccordion
+							actionBtn='participar'
+							src={icon1}
+							title='Programa3'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+						<SimpleAccordion
+							actionBtn='participar'
+							src={icon1}
+							title='Programa4'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+						<SimpleAccordion
+							actionBtn='participar'
+							src={icon1}
+							title='Programa5'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+						<SimpleAccordion
+							actionBtn='participar'
+							src={icon1}
+							title='Programa6'
+							description='Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.'
+						></SimpleAccordion>
+					</Stack>
+				)}
 			</Stack>
-			<Stack></Stack>
+			<Stack
+				sx={{
+					color: '#FFFFFF',
+					fontWeight: '700',
+					margin: '0 auto',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<p>Powered by squad Minsur</p>
+			</Stack>
 		</div>
 	);
 };

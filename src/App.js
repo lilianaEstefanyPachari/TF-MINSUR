2ed01ec3c24220e94e9e7d2babc066f1a8983566
Nay Trevejo <nay.trevejo@gmail.com>
2022-08-24 01:32:13 -0500

    added calendar views

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
-import { AuthProvider } from './context/authContext';
 import TimeCoupon from './components/timeCoupon/TimeCoupon';
+import BirthdayCoupon from './components/birthdayCoupon/BirthdayCoupon';
+import { AuthProvider } from './context/authContext';
 
 function App() {
 	return (
@@ -13,7 +14,6 @@ function App() {
 					<Routes>
 						<Route path='/' element={<Login />} />
 						<Route path='/home' element={<Home />} />
-						<Route path='/formFreeTime' element={<TimeCoupon />} />
 					</Routes>
 				</BrowserRouter>
 			</AuthProvider>
